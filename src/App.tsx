867433a20865cdc3d417305f3a8d9bcbc84fad90
FilipeBzr <fil@pe.com>
2025-02-03 15:54:37 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl text-accent font-bold text-center mt-8">
+      Configuração do Tailwind OK! ✅
+    </h1>
+  )
+}
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#2A2A2A',
+        'accent': '#E89F71',
+      }
+    },
+  },
+  plugins: [],
+}
